85d1054f558f30e4e3b3e7425d75971eb0b68b5e
Corbean Mircea <corbeanm@gmail.com>
2023-10-23 14:15:37 +0300

    Completed Layout

--- components/library.tsx
+++ components/library.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import{ TbPlaylist } from "react-icons/tb";
+import{ AiOutlinePlus} from "react-icons/ai"
 
 const Library = () =>{
     const onClick = () => {
@@ -23,8 +24,33 @@ const Library = () =>{
                     gap-x-2
                 "
                 >
-                    <TbPlaylist/>
+                    <TbPlaylist className="text-neutral-400" size={26}/>
+                    <p
+                    className="text-neutral-400
+                    font-medium
+                    text-md
+                    "
+                    >
+                        Your Library
+                    </p>
                 </div>
+                <AiOutlinePlus
+                onclick={onClick}
+                size={20}
+                className="text-neutral-400
+                cursor-pointer
+                hover:text-white
+                transition"
+                />
+            </div>
+            <div className="
+            flex
+            felx-col
+            gap-y-2
+            mt-4
+            px-3
+            ">
+                List of Songs!
             </div>
         </div>
     );
